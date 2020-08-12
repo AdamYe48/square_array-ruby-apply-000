08152c171d421290226eb9f110451119690c3ad4
Adam Ye <adamye48@gmail.com>
2020-08-12 13:58:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,7 @@
- def square_array(array)
-array = ["1", "2", "3"]
- new_array = []
-
- array.each do |integer|
-  new_array << integer * 
- new_array
-end
-
+def square_array(array)
+    new_array = []
+    array.each do |element|
+        new_array << element ** 2
+    end
+    new_array
 end
